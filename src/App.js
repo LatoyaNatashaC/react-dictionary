d0b6b7a1af6919c11d64cb564394d5a79ee59be0
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-10-15 12:11:24 -0700

    component created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import shecodeslogo from "./shecodeslogo.png";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
@@ -8,6 +9,9 @@ export default function App() {
         <header className="App-header">
           <img src={shecodeslogo} className="App-logo" alt="logo" />
         </header>
+        <main>
+          <Dictionary />
+        </main>
         <footer className="text-center">
           This project was coded by Latoya Natasha Campbell and is open sourced
           on GitHub and hosted by Netlify
