994f436887b677f8a34d9b87c9a15ea30d9b7ee8
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-10-15 12:01:11 -0700

    footer added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import shecodeslogo from "./shecodeslogo.png";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={shecodeslogo} className="App-logo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={shecodeslogo} className="App-logo" alt="logo" />
+        </header>
+        <footer className="text-center">
+          This project was coded by Latoya Natasha Campbell and is open sourced
+          on GitHub and hosted by Netlify
+        </footer>
+      </div>
     </div>
   );
 }
-
-App;
