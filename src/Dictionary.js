53f3fdaddff537b9bc68fc3a36fb0eb752c55541
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-10-16 13:06:21 -0700

    new function created

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,12 +5,16 @@ import axios from "axios";
 export default function Dictionary() {
   let [keyWord, setKeyWord] = useState("");
 
+  function handleResponse(response) {
+    console.log();
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for the definition of ${keyWord}`);
 
     let apiKey = "4f3b0tf3219b4c7758082d0o48eabbbe";
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word={word}&key=${apiKey}`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyWord}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
