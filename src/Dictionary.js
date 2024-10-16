863d400e93728b2ab569769eceab0826cb082a79
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-10-16 12:54:44 -0700

    api url & key integrated

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,6 +8,10 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
     alert(`Searching for the definition of ${keyWord}`);
+
+    let apiKey = "4f3b0tf3219b4c7758082d0o48eabbbe";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word={word}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
