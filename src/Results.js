9977e288c6fe9ed8466109d992d5bd80f4467c5b
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-10-17 17:09:12 -0700

    map created

--- src/Results.js
+++ src/Results.js
@@ -3,7 +3,12 @@ import React from "react";
 export default function Results(props) {
   console.log(props.results);
   if (props.results) {
-    return <div className="Results">Hello from results</div>;
+    return (
+      <div className="Results">
+        <h2>{props.results.word}</h2>
+        {props.results.meanings.map(function (meanings, index) {})}
+      </div>
+    );
   } else {
     return null;
   }
